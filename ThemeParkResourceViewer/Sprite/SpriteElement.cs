93553aee7933ffe16a15f9fc0cda7cafdaf3c363
tigrouind <tigrou.ind@gmail.com>
2014-01-25 18:05:19 +0100

    Initial commit

---
+++ ThemeParkResourceViewer/Sprite/SpriteElement.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ThemeParkResourceViewer
+{
+	class SpriteElement
+	{
+		public ushort Sprite;
+		public short XOffset;
+		public short YOffset;
+		public ushort XFlipped;
+		public ushort Next;
+	};
+}
